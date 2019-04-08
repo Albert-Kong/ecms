40baf8ddd255fa0332d55ef385bd914e5269bcfe
leo <1579858856@qq.com>
2019-04-08 11:34:28 +0800

    4.8

--- src/main/java/com/zs/ecms/EcmsApplication.java
+++ src/main/java/com/zs/ecms/EcmsApplication.java
@@ -1,15 +1,18 @@
 package com.zs.ecms;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 @MapperScan("com.zs.ecms.mapper")
 public class EcmsApplication {
+
+
     public static void main(String[] args) {
-        SpringApplication.run(EcmsApplication.class, args);
+       SpringApplication.run(EcmsApplication.class,args);
     }
 
 }
